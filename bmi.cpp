7fb4cfe726d3d920e846c9310d467d2b6f09e8f4
MOMIN ZAHOOR <98146838+CodeWithMomin@users.noreply.github.com>
2023-12-09 10:41:37 +0530

    Update bmi.cpp
    hello my self momin, here i proposed some changes for better results . hope you will allow it.

--- bmi.cpp
+++ bmi.cpp
@@ -5,22 +5,24 @@ using namespace std;
 int main()
 {
     float bmi;
-    float pounds;
+    float kilos;
     float height;
 
     cout << "Enter your weight in pounds : " << endl;
-    cin >> pounds;
+    cin >> kilos;
 
     cout << "Enter your height in inches : " << endl;
     cin >> height;
-
+   float pounds=kg*2.205;   // here converting the kilograms into pounds for better conversion
+    float inchs=height/2.54; // here converting the centimeters into inchs for better conversion
+    
     if (pounds <= 0 || height <= 0)
     {
         cout << "Invalid input";
     }
     else
     {
-        bmi = pounds / (height * height)*703; //Formula: weight (lb) / [height (in)]2 x 703
+        bmi = (pounds / (inchs * inchs))*703; //Formula: weight (lb) / [height (in)]2 x 703      ----------- i used somw braces for better calcultion
 
         cout << "Your BMI is : " << bmi << endl;
     }
