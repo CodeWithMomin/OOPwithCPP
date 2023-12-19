789bd15bcb1460a920b346393d7b3720f8f852dd
arsalankazmi7 <arsalankazmi7@gmail.com>
2023-12-19 22:32:19 +0530

    Minor spelling correction

--- temp-conversion.cpp
+++ temp-conversion.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main(){
 	float celcius, fahrenheit;
-	cout << "Enter temperatur in Celcius: ";
+	cout << "Enter temperature in Celcius: ";
 	cin >> celcius;
 	fahrenheit = (celcius * 9 / 5) + 32;
 	cout << celcius << "C is equivalent to " << fahrenheit << "F" << endl;
